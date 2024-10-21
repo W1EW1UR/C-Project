37489bd2af8da38215cb9dbac2e179c5d326b833
W1EW1UR <kapimuzyk@gmail.com>
2024-10-21 21:23:04 +0200

    upload projektu

---
+++ Projekt/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Projekt_FILE C:/Users/kapim/Desktop/procpp/C-Project/Projekt/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/Projekt.exe)
+set(__QT_DEPLOY_TARGET_Projekt_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Projekt_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
--- README.md
+++
@@ -1,1 +0,0 @@
-# Sysyem Medyczny
